04b73fc6be71f48f641f2979e89c8ec303c895be
IvanPavlovets <ivan150287@gmail.com>
2022-11-02 13:59:50 +0700

    5. Аутентификация в проекте TODO [#504877] - fix import

--- src/main/java/ru/job4j/todo/controller/UserContoller.java
+++ src/main/java/ru/job4j/todo/controller/UserContoller.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
-import static ru.job4j.todo.utils.UserUtils.getUserSession;
+import static ru.job4j.todo.utils.*;
 
 @Controller
 @AllArgsConstructor
